ffcb01c5e011e70f82dc00826aa36adaf42f5eda
Matt Gerst <mattgerst@gmail.com>
2018-04-04 13:34:54 -0500

    Also attempt to load from /etc/flagrc

--- flag_bearer/config.py
+++ flag_bearer/config.py
@@ -11,10 +11,12 @@ class Config(ConfigParser):
     def load(cls, noflagrc=False):
         # Load configuration files in order
         # - default.ini
+        # - /etc/flagrc
         # - ~/.flagrc
         conf = cls()
         conf.read(join(ROOT, 'default.ini'))
-        
+        conf.read('/etc/flagrc')
+
         # Skip loading the flagrc, only used in tests
         if noflagrc:
             return conf
